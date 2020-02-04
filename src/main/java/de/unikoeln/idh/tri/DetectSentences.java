5456741f8e0bcc876f01acf8966ea2d84fd435fc
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-02-04 08:02:02 +0100

    empty classes

---
+++ src/main/java/de/unikoeln/idh/tri/DetectSentences.java
@@ -0,0 +1,15 @@
+package de.unikoeln.idh.tri;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.jcas.JCas;
+
+public class DetectSentences extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/de/unikoeln/idh/tri/Main.java
@@ -0,0 +1,10 @@
+package de.unikoeln.idh.tri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
